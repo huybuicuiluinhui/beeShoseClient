20c4261817c9d704ea6fbff50105817a4c131e89
huybuicuiluinhui <huybuithezz0610@gmail.com>
2023-08-13 20:37:05 +0700

    add src

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -371,7 +371,7 @@ const Header = () => {
                             htmlFor="email"
                             className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                           >
-                            Số điện thoại
+                            Số điện thoại hoặc Email
                           </label>
                           <input
                             type="text"
@@ -418,7 +418,10 @@ const Header = () => {
                             </div>
                           </div>
                           <a
-                            href="#"
+                            onClick={() => {
+                              setTypeModal(3);
+                              setShowToast(false);
+                            }}
                             className="text-sm font-medium text-purple-600 hover:underline dark:text-purple-500"
                           >
                             Quên mật khẩu?
@@ -450,7 +453,7 @@ const Header = () => {
                   </div>
                 </div>
               </section>
-            ) : (
+            ) : typeModal === 2 ? (
               <section className="bg-gray-50 dark:bg-gray-900">
                 <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto  lg:py-0">
                   <a
@@ -475,7 +478,7 @@ const Header = () => {
                             htmlFor="email"
                             className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                           >
-                            Số điện thoại
+                            Số điện thoại hoặc Email
                           </label>
                           <input
                             type="text"
@@ -551,6 +554,125 @@ const Header = () => {
                           }}
                         >
                           Tạo tài khoản
+                        </button>
+                        <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                          Bạn đã có tài khoản?{" "}
+                          <a
+                            onClick={() => {
+                              setTypeModal(1);
+                              setShowToast(false);
+                            }}
+                            className="font-medium text-purple-600 hover:underline dark:text-purple-500"
+                          >
+                            Đăng nhập ngay
+                          </a>
+                        </p>
+                      </form>
+                    </div>
+                  </div>
+                </div>
+              </section>
+            ) : (
+              <section className="bg-gray-50 dark:bg-gray-900">
+                <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto  lg:py-0">
+                  <a
+                    href="#"
+                    className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
+                  >
+                    <img
+                      className="w-20 h-20 mr-2 object-contain"
+                      src={Images.iconLogo}
+                      alt="logo"
+                    />
+                    BeeShoes
+                  </a>
+                  <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700 mb-4">
+                    <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                      <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                        Quên mật khẩu
+                      </h1>
+                      <form className="space-y-4 md:space-y-6" action="#">
+                        <div>
+                          <label
+                            htmlFor="email"
+                            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                          >
+                            Số điện thoại hoặc Email
+                          </label>
+                          <input
+                            type="text"
+                            name="email"
+                            id="email"
+                            className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-purple-600 focus:border-purple-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            required
+                          />
+                        </div>
+                        <div>
+                          <label
+                            htmlFor="password"
+                            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                          >
+                            Mật khẩu
+                          </label>
+                          <input
+                            type="password"
+                            name="password"
+                            id="password"
+                            placeholder="••••••••"
+                            className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-purple-600 focus:border-purple-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            required
+                          />
+                        </div>
+                        <div>
+                          <label
+                            htmlFor="confirm-password"
+                            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                          >
+                            Nhập lại mật khẩu
+                          </label>
+                          <input
+                            type="confirm-password"
+                            name="confirm-password"
+                            id="confirm-password"
+                            placeholder="••••••••"
+                            className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-purple-600 focus:border-purple-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            required
+                          />
+                        </div>
+                        <div className="flex items-start">
+                          <div className="flex items-center h-5">
+                            <input
+                              id="terms"
+                              aria-describedby="terms"
+                              type="checkbox"
+                              className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-purple-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-purple-600 dark:ring-offset-gray-800"
+                              required
+                            />
+                          </div>
+                          <div className="ml-3 text-sm">
+                            <label
+                              htmlFor="terms"
+                              className="font-light text-gray-500 dark:text-gray-300"
+                            >
+                              Tôi đồng ý với các{" "}
+                              <a
+                                className="font-medium text-purple-600 hover:underline dark:text-purple-500"
+                                href="#"
+                              >
+                                điều khoản
+                              </a>
+                            </label>
+                          </div>
+                        </div>
+                        <button
+                          type="submit"
+                          className="w-full text-white bg-purple-600 hover:bg--700 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-800"
+                          onClick={() => {
+                            setTypeModal(3);
+                            setShowToast(true);
+                          }}
+                        >
+                          Gửi xác nhận
                         </button>
                         <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                           Bạn đã có tài khoản?{" "}
